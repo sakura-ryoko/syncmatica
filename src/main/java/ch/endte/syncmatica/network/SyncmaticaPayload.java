93f03dd3beece9c248a56f29e5139d8a0322ef86
Sakura Ryoko <sakura@ryoko.soy>
2024-01-19 00:06:49 -0500

    Mojang Mappings toying (From CarpetMod --)

--- src/main/java/ch/endte/syncmatica/network/SyncmaticaPayload.java
+++ src/main/java/ch/endte/syncmatica/network/SyncmaticaPayload.java
@@ -1,17 +1,33 @@
 package ch.endte.syncmatica.network;
 
-import net.minecraft.network.PacketByteBuf;
-import net.minecraft.network.packet.CustomPayload;
-import net.minecraft.util.Identifier;
+import net.minecraft.nbt.CompoundTag;
+//import net.minecraft.network.PacketByteBuf; --> FriendlyByteBuf
+import net.minecraft.network.FriendlyByteBuf;
+//import net.minecraft.network.packet.CustomPayload; --> CustomPacketPayload
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+//import net.minecraft.util.Identifier; --> ResourceLocation
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
 
-public record SyncmaticaPayload(PacketByteBuf byteBuf, Identifier id) implements CustomPayload {
+public record SyncmaticaPayload(CompoundTag data) implements CustomPacketPayload {
+    public static final StreamCodec<FriendlyByteBuf, SyncmaticaPayload> STREAM_CODEC = CustomPacketPayload.codec(SyncmaticaPayload::write, SyncmaticaPayload::new);
+    public static final ResourceLocation SYNCMATICA_CHANNEL = new ResourceLocation("syncmatica", "hello");
+    public static final Type<SyncmaticaPayload> TYPE = new CustomPacketPayload.Type<>(SYNCMATICA_CHANNEL);
+    /*public SyncmaticaPayload(ResourceLocation identifier, FriendlyByteBuf input) {
+        this(new FriendlyByteBuf(input.readBytes(input.readableBytes())), identifier);
+    }*/
 
-    public SyncmaticaPayload(Identifier identifier, PacketByteBuf input) {
-        this(new PacketByteBuf(input.readBytes(input.readableBytes())), identifier);
+    public SyncmaticaPayload(FriendlyByteBuf input) {
+        this(input.readNbt());
     }
 
+    //@Override
+    public void write(FriendlyByteBuf output) {
+        output.writeNbt(data);
+    }
     @Override
-    public void write(PacketByteBuf buf) {
-        buf.writeBytes(byteBuf);
+    public @NotNull Type<SyncmaticaPayload> type() {
+        return TYPE;
     }
 }
