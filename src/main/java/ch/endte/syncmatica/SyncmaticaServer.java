b6292c1acdf42e5ffa2b81d0b57e48989a3d31fa
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2024-02-03 19:05:41 -0500

    24w04a - Some notes and cleanup

--- src/main/java/ch/endte/syncmatica/SyncmaticaServer.java
+++ src/main/java/ch/endte/syncmatica/SyncmaticaServer.java
@@ -3,7 +3,7 @@ package ch.endte.syncmatica;
 import net.fabricmc.api.DedicatedServerModInitializer;
 
 /**
- * Launches preInit() for Dedicated servers
+ * Launches preInit() for Dedicated servers (And sets the MOD_ENV / MOD_VERSION, etc.)
  */
 public class SyncmaticaServer implements DedicatedServerModInitializer
 {
