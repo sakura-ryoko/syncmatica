eaf0836bfa1b06c57e5581cbda444469978999ac
Sakura Ryoko <sakura@ryoko.soy>
2024-11-02 08:16:53 -0400

    fix mixin transformation error (?)

--- src/main/java/ch/endte/syncmatica/litematica_mixin/MixinSchematicPlacement.java
+++ src/main/java/ch/endte/syncmatica/litematica_mixin/MixinSchematicPlacement.java
@@ -52,8 +52,16 @@ public abstract class MixinSchematicPlacement implements IIDContainer, MovingFin
         }
     }
 
+    /*
     @Inject(method = "<init>(Lfi/dy/masa/litematica/schematic/LitematicaSchematic;Lnet/minecraft/util/math/BlockPos;Ljava/lang/String;ZZLfi/dy/masa/litematica/schematic/placement/SchematicPlacementManager;)V", at = @At("TAIL"), remap = false)
     public void setNull(LitematicaSchematic schematic, BlockPos origin, String name, boolean enabled, boolean enableRender, SchematicPlacementManager placementManager, CallbackInfo ci) {
+        serverId = null;
+    }
+     */
+
+    @Inject(method = "<init>*", at = @At("TAIL"), remap = false)
+    public void setNull(final LitematicaSchematic schematic, final BlockPos origin, final String name, final boolean enabled, final boolean enableRender, final CallbackInfo ci)
+    {
         serverId = null;
     }
 
