19dccd1b5440d3f44782cefa18c48557fb9c53fd
Sakura Ryoko <sakura@ryoko.soy>
2025-04-05 16:04:24 -0400

    Port fixes to 1.21.4

--- src/main/java/ch/endte/syncmatica/litematica/gui/WidgetListSyncmaticaServerPlacement.java
+++ src/main/java/ch/endte/syncmatica/litematica/gui/WidgetListSyncmaticaServerPlacement.java
@@ -21,7 +21,7 @@ import fi.dy.masa.malilib.gui.interfaces.ISelectionListener;
 import fi.dy.masa.malilib.gui.widgets.WidgetListBase;
 import fi.dy.masa.malilib.gui.widgets.WidgetSearchBar;
 import fi.dy.masa.malilib.render.RenderUtils;
-import fi.dy.masa.malilib.util.data.Schema;
+import fi.dy.masa.malilib.util.Schema;
 import fi.dy.masa.malilib.util.StringUtils;
 import fi.dy.masa.litematica.gui.Icons;
 import fi.dy.masa.litematica.schematic.SchematicSchema;
