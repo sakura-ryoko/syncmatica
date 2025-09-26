5cfd3175f364dbc483db5407231410f04ed90900
Sakura Ryoko <sakura@ryoko.soy>
2025-09-26 12:16:58 -0400

    Bump RC.1, update Schema.java

--- src/main/java/ch/endte/syncmatica/litematica/schematic/Schema.java
+++ src/main/java/ch/endte/syncmatica/litematica/schematic/Schema.java
@@ -25,7 +25,8 @@ public enum Schema implements StringIdentifiable
     // TODO --> Add Schema Versions to this as versions get released
     // Minecraft Data Versions
 	SCHEMA_FUTURE  (9999, "FUTURE"),
-	SCHEMA_25W35A  (4542, "25w35a"),
+    SCHEMA_1_21_09 (4553, "1.21.9-rc1"),
+    SCHEMA_25W36B  (4546, "25w36b"),
 	SCHEMA_25W31A  (4534, "25w31a"),
     SCHEMA_1_21_08 (4440, "1.21.8"),
     SCHEMA_1_21_07 (4438, "1.21.7"),
