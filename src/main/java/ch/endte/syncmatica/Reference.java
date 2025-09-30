ad07d21a304705a800ca8b9e01212af475a8ec7a
Sakura Ryoko <sakura@ryoko.soy>
2025-09-30 12:49:13 -0400

    Bump to release

--- src/main/java/ch/endte/syncmatica/Reference.java
+++ src/main/java/ch/endte/syncmatica/Reference.java
@@ -4,6 +4,7 @@ import java.nio.file.Path;
 import ch.endte.syncmatica.util.StringTools;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.SharedConstants;
 
 /**
  * Main (Generic) Reference Calls --
@@ -15,6 +16,7 @@ public class Reference
     public static final String MOD_ID = "syncmatica";
     public static final String MOD_NAME = "Syncmatica";
     public static final String MOD_VERSION = StringTools.getModVersion(MOD_ID); // No more manually typing in the version # :)
+    public static final String MC_VERSION = SharedConstants.getGameVersion().id();
     private static final EnvType MOD_ENV = FabricLoader.getInstance().getEnvironmentType();
     public static final boolean MOD_DEBUG = false;
 
